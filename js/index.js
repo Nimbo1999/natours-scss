5ff893f6c35dc8f26c31a099229c2b1ab46fd951
Matheus Lopes <=>
2022-09-16 16:27:48 -0300

    Finish the booking section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 for(const element of document.getElementsByTagName('a')) {
     element.addEventListener('click', event => event.preventDefault());
 }
+
+for(const element of document.getElementsByTagName('form')) {
+    element.addEventListener('submit', event => {
+        event.preventDefault();
+        console.log({ target: event.target });
+    });
+}
