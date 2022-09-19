ceb92c0eba4eef4638b7daa6ec3028cc1ce46609
Matheus Lopes <=>
2022-09-19 14:17:58 -0300

    Add the popup section

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 for(const element of document.getElementsByTagName('a')) {
-    element.addEventListener('click', event => event.preventDefault());
+    element.addEventListener('click', event => {
+        if (event.target.hash && !!document.querySelector(event.target.hash)) {
+            /**
+             * @type {HTMLElement}
+             */
+            const element = document.querySelector(event.target.hash);
+            window.scrollTo({ behavior: 'smooth', top: element.offsetTop});
+        }
+    });
 }
 
 for(const element of document.getElementsByTagName('form')) {
     element.addEventListener('submit', event => {
         event.preventDefault();
-        console.log({ target: event.target });
     });
 }
